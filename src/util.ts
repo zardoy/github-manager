ca2603b02b56010d8d96dd014641c78800577b22
Vitaly Turovsky <vital2580@icloud.com>
2021-09-08 07:22:28 +0300

    style: run prettier

--- src/util.ts
+++ src/util.ts
@@ -1,68 +1,65 @@
-import path from 'path';
-import { getGithubRemoteInfo } from 'github-remote-info';
-import _ from 'lodash';
-import vscode from 'vscode';
-import { getDirsFromCwd } from './utils/git';
+import path from 'path'
+import { getGithubRemoteInfo } from 'github-remote-info'
+import _ from 'lodash'
+import vscode from 'vscode'
+import { getDirsFromCwd } from './utils/git'
 
 // actually copy-pasted from zardoy/rename-repos/src/common.ts
 
-export const getWhereToOpen = (): string => vscode.workspace.getConfiguration('github-manager').get('whereToOpen')!;
+export const getWhereToOpen = (): string => vscode.workspace.getConfiguration('github-manager').get('whereToOpen')!
 
 export interface GithubRepo {
-    owner: string;
-    name: string;
+    owner: string
+    name: string
     /** Relative directory path from defaultCloneDirectory */
-    dirPath: string;
+    dirPath: string
 }
 
 export function getReposDir() {
-    const gitDefaultDir: string | undefined | null = vscode.workspace.getConfiguration('git').get('defaultCloneDirectory');
-    if (!gitDefaultDir)
-        throw new Error('Ensure that git.defaultCloneDirectory setting is pointing to directory with your GitHub repos');
+    const gitDefaultDir: string | undefined | null = vscode.workspace.getConfiguration('git').get('defaultCloneDirectory')
+    if (!gitDefaultDir) throw new Error('Ensure that git.defaultCloneDirectory setting is pointing to directory with your GitHub repos')
 
-    return gitDefaultDir;
+    return gitDefaultDir
 }
 
 export const getGithubRepos = async () => {
     try {
-        console.time('Show selections');
-        const gitDefaultDir = getReposDir();
+        console.time('Show selections')
+        const gitDefaultDir = getReposDir()
 
-        const { git: gitDirs } = await getDirsFromCwd(gitDefaultDir);
-        const dirsOriginInfo = await Promise.allSettled(
-            gitDirs.map(async dir => getGithubRemoteInfo(path.join(gitDefaultDir, dir))),
-        );
+        const { git: gitDirs } = await getDirsFromCwd(gitDefaultDir)
+        const dirsOriginInfo = await Promise.allSettled(gitDirs.map(async dir => getGithubRemoteInfo(path.join(gitDefaultDir, dir))))
         const reposWithGithubInfo = dirsOriginInfo
             .map((state, index): GithubRepo | undefined => {
-                if (state.status === 'fulfilled') return state.value ? { ...state.value, dirPath: gitDirs[index] } : undefined;
+                if (state.status === 'fulfilled') return state.value ? { ...state.value, dirPath: gitDirs[index] } : undefined
 
-                return undefined;
+                return undefined
             })
-            .filter(Boolean) as GithubRepo[];
-        const ownerCountMap = _.countBy(reposWithGithubInfo, r => r.owner);
+            .filter(Boolean) as GithubRepo[]
+        const ownerCountMap = _.countBy(reposWithGithubInfo, r => r.owner)
         // TODO sort also by name
-        const sortedRepos = _.sortBy(reposWithGithubInfo, r => ownerCountMap[r.owner]).reverse();
+        const sortedRepos = _.sortBy(reposWithGithubInfo, r => ownerCountMap[r.owner]).reverse()
 
-        return sortedRepos;
+        return sortedRepos
     } finally {
-        console.timeEnd('Show selections');
+        console.timeEnd('Show selections')
     }
-};
+}
 
 export const openSelectedDirectory = async (dirPath: GithubRepo['dirPath'], forceOpenNewWindow?: boolean) => {
-    const gitDefaultDir = getReposDir();
+    const gitDefaultDir = getReposDir()
 
-    const whereToOpen = getWhereToOpen();
+    const whereToOpen = getWhereToOpen()
 
-    const folderUri = vscode.Uri.file(path.join(gitDefaultDir, dirPath));
+    const folderUri = vscode.Uri.file(path.join(gitDefaultDir, dirPath))
     const forceNewWindow = (() => {
-        if (forceOpenNewWindow !== undefined) return forceOpenNewWindow;
-        if (whereToOpen === 'alwaysSameWindow') return false;
-        if (whereToOpen === 'newWindowIfNotEmpty') return vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0;
-        return false;
-    })();
+        if (forceOpenNewWindow !== undefined) return forceOpenNewWindow
+        if (whereToOpen === 'alwaysSameWindow') return false
+        if (whereToOpen === 'newWindowIfNotEmpty') return vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+        return false
+    })()
 
     await vscode.commands.executeCommand('vscode.openFolder', folderUri, {
         forceNewWindow,
-    });
-};
+    })
+}
